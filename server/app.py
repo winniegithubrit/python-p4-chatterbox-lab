c648abc9becaf6c053e7cdf7040e0547df497bd4
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-06-23 15:28:34 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -10,17 +8,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.all()
+        return jsonify([message.to_dict() for message in messages])
+
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'username' not in data or 'body' not in data:
+            return jsonify({'error': 'Missing username or body'}), 200
+
+        message = Message(username=data['username'], body=data['body'])
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def message_by_id(id):
+    message = db.session.get(Message, id)
+    if message is None:
+        return jsonify({'error': 'Message not found'}), 404
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(message, attr, value)
+        db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'message': 'Message deleted successfully'}), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555)
